3f496fc52eede5d2fd1e6b5c714b80d3ac54d5cf
Anshu Mishra <anshu.mishra@xolo.in>
2016-03-03 13:22:56 +0530

    Blame the night

--- Logger.sh
+++ Logger.sh
@@ -2,3 +2,4 @@ touch Releasenotes.txt
 #echo ${CHANGES_SINCE_LAST_SUCCESS, reverse=true, format="\nBuild #%n\n%c\n\n"} >> Releasenotes.txt
 printf "$changes" >> Releasenotes.txt
 mkdir test
+blame the night
